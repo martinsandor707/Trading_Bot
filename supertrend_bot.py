7e36a5a20537041c43484fcad15913f8265f510c
Sándor Martin Tibor <martinsandor707@gmail.com>
2024-07-19 23:24:34 +0200

    Create 1.0 version of supertrend bot

--- supertrend_bot.py
+++ supertrend_bot.py
@@ -4,8 +4,9 @@ Created on Thu Jul 18 17:36:21 2024
 
 @author: marti
 """
-
+import time
 import json
+import warnings
 import numpy as np
 import pandas as pd
 import pandas_ta as ta
@@ -19,6 +20,15 @@ class Bitvavo_Supertrend:
     period = None
     st_length = None
     st_factor = None
+    margin = 0.5    #This HAS to be between 0 and 1
+    """
+    When looking at eg.: the "BTC-EUR" ticker, 
+    "BTC" is the base currency
+    "EUR" is the quote currency
+    !!! Whenever placing market orders, the buy/sell amount can be specified in EITHER currency
+    """
+    base_currency = None
+    quote_currency = None
     
     def __init__(self, ticker, period, st_length, st_factor):
         keys=json.load(open("api.keys"))
@@ -34,20 +44,58 @@ class Bitvavo_Supertrend:
         self.st_length = st_length
         self.st_factor = st_factor
         
+        self.base_currency = ticker.split('-')[0]
+        self.quote_currency = ticker.split('-')[1]
         
     # Handle errors.
     def error_callback(self, error):
-        print("Something unexpected happened.")
         print("Errors:", json.dumps(error, indent=2))
-        raise Exception(json.dumps(error, indent=2))
         
     # Retrieve the data you need from Bitvavo in order to implement your
     # trading logic. Use multiple workflows to return data to your
     # callbacks.
     def a_trading_strategy(self):
-        data = self.get_Data(self.ticker, self.period)
-        return data
-        
+        data = self.get_data(self.ticker, self.period)
+        print(f"Downloaded latest candle chart for {self.ticker} with a period of {self.period}")
+        signal = self.print_signal(data)
+        if signal == 'Buy':
+            #print(f"{data.iloc[-1].name} If this weren't a test run, I would have bought {self.base_currency}")
+            
+            balance_response = bvavo.bitvavo_engine.balance({'symbol': f'{self.quote_currency}'})
+            print(f"Wallet details received for {self.quote_currency}")
+            
+            amount_to_buy = str(balance_response[0]['available']*self.margin)
+            order_response = bvavo.bitvavo_engine.placeOrder(self.ticker, "buy", "market", {'amountQuote' : amount_to_buy*0.5})
+            if order_response['orderId'] is not None:
+                print(f"Successfully bought {self.base_currency} for {amount_to_buy} {self.quote_currency}")
+                
+            
+        elif signal == 'Sell':
+            #print(f"{data.iloc[-1].name} If this weren't a test run, I would have sold {self.base_currency}")
+            
+            balance_response = bvavo.bitvavo_engine.balance({'symbol': f'{self.base_currency}'})
+            print(f"Wallet details received for {self.base_currency}")
+            
+            amount_to_sell = balance_response[0]['available']
+            order_response = bvavo.bitvavo_engine.placeOrder(self.ticker, "sell", "market", {'amount': amount_to_sell})
+            if order_response['orderId'] is not None:
+                print(f"Successfully bought {self.base_currency} for {amount_to_sell} {self.quote_currency}")
+                
+                
+        else:
+            #print(f"{data.iloc[-1].name} If this weren't a test run, I would have done nothing anyway")
+            print(f"{data.iloc[-1].name} No signal")
+    
+    def print_signal(self, data):
+        if self.buy_condition(data):
+            print(f"{data.iloc[-1].name} Buy signal for {data.iloc[-1]['close']} {self.quote_currency}")
+            return "Buy"
+        elif self.sell_condition(data):
+            print(f"{data.iloc[-1].name} Sell signal for {data.iloc[-1]['close']} {self.quote_currency}")
+            return "Sell"
+        else:
+            print(f"{data.iloc[-1].name} No signal for {data.iloc[-1]['close']} {self.quote_currency}")
+            return "Do nothing"
         
     def buy_condition(self, data):
         return data.iloc[-1]['close'] > data.iloc[-1][f'ST_{self.st_length}_{self.st_factor}'] and data.iloc[-2]['close'] < data.iloc[-2][f'ST_{self.st_length}_{self.st_factor}']
@@ -55,12 +103,31 @@ class Bitvavo_Supertrend:
     def sell_condition(self, data):
         return data.iloc[-1]['close'] < data.iloc[-1][f'ST_{self.st_length}_{self.st_factor}'] and data.iloc[-2]['close'] > data.iloc[-2][f'ST_{self.st_length}_{self.st_factor}']
         
+    
+    
+    """
+    Convenience method for backtesting on latest data.
+    """
+    def print_all_signals(self, data):
+        prev_row = None
+        profit_pct = 1.0
+        trades = []
+        last_trade = None
+        for index, row in data.iterrows():
+            if prev_row is not None and row['close'] > row[f'ST_{self.st_length}_{self.st_factor}'] and prev_row['close'] < prev_row[f'ST_{self.st_length}_{self.st_factor}']:
+                print(f'{index} {self.ticker} Buy signal for {row.close} EUR')
+                trades.append({'Date' : index, 'Action': 'Buy', 'Price': row.close})
+            
+            elif prev_row is not None and row['close'] < row[f'ST_{self.st_length}_{self.st_factor}'] and prev_row['close'] > prev_row[f'ST_{self.st_length}_{self.st_factor}']:
+                print(f'{index} {self.ticker} Sell signal for {row.close} EUR')
+                trades.append({'Date' : index, 'Action': 'Sell', 'Price': row.close})
+            prev_row = row
         
-    def get_Balance(self, asset):
-        response = self.bitvavo_engine.balance({'symbol': asset})
-        return response
-        
-        
+        for trade in trades:
+            if last_trade is not None and trade['Action'] == 'Sell' and last_trade['Action'] == 'Buy':
+                profit_pct = profit_pct * (trade['Price']/last_trade['Price'])
+            last_trade = trade
+        print(f"Profit in the past {len(data)} periods is {(profit_pct-1)*100}%")
         
         
     """
@@ -68,7 +135,7 @@ class Bitvavo_Supertrend:
         Calculates the supertrend based on heikin-ashi candles, and appends
         it to the end of the dataframe.
     """
-    def get_Data(self, ticker, period):
+    def get_data(self, ticker, period):
         data=self.bitvavo_engine.candles(ticker, period)
         data=pd.DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
         data['timestamp'] = pd.to_datetime(data['timestamp'], unit='ms')
@@ -87,14 +154,29 @@ class Bitvavo_Supertrend:
         data=pd.concat([data, ta.overlap.supertrend(heikin_ashi['HA_High'], heikin_ashi['HA_Low'], heikin_ashi['HA_Close'], self.st_length, self.st_factor)[f'SUPERT_{self.st_length}_{self.st_factor}']], axis=1)
         data.columns = ['open', 'high', 'low', 'close', 'volume', f'ST_{self.st_length}_{self.st_factor}']
         
-        return data
+        return data.iloc[self.st_length:]
         
     
 ############################ Class ends here ############################
+warnings.simplefilter(action='ignore')
+ticker="PEPE-EUR"
+period = "30m"
+bvavo = Bitvavo_Supertrend(ticker=ticker, period=period, st_length= 20, st_factor=4.0)
 
-bvavo = Bitvavo_Supertrend(ticker="BTC-EUR", period="30m", st_length= 20, st_factor=4.0)
-wallet = bvavo.get_Balance('BTC')
-current_coins = float(wallet[0]['available'])
-data=bvavo.a_trading_strategy()
-print(bvavo.bitvavo_engine.getRemainingLimit())
-bvavo.bitvavo_socket.closeSocket()
+try:
+    while True:
+        bvavo.a_trading_strategy()
+        print(bvavo.bitvavo_engine.getRemainingLimit())
+        print("------------------------------------------------------------------------")
+        time.sleep(10)
+except KeyboardInterrupt:
+    print("Program interupted by user")
+    
+finally:
+    print("Finally closing websocket")
+    bvavo.bitvavo_socket.closeSocket()
+
+
+
+#%% Testing area
+#bvavo.print_all_signals(bvavo.get_data(ticker, period))
